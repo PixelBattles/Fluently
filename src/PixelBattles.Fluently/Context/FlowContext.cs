5d2b4ec366cc18d75f80a55cb4e8ac1d4cb3753d
Ihnat Klimchuk <ihnat.klimchuk@gmail.com>
2018-04-14 01:19:14 +0300

    Adding flow context

---
+++ src/PixelBattles.Fluently/Context/FlowContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixelBattles.Fluently.Context
+{
+    public class FlowContext : IFlowContext
+    {
+    }
+}
---
+++ src/PixelBattles.Fluently/FlowTest.cs
@@ -0,0 +1,12 @@
+﻿using PixelBattles.Fluently.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixelBattles.Fluently
+{
+    public abstract class FlowTest
+    {
+        public abstract IFlowContext Run(IFlowContext context);
+    }
+}
